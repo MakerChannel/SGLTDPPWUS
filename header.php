531867eb39a37893453d0059ec180f84bc8b5bf2
MakerChannel <68184436+MakerChannel@users.noreply.github.com>
2021-02-01 21:52:46 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+<link rel="shortcut icon" href="https://lh3.googleusercontent.com/2BJnJprT0Hw6qvid7J55wGus1A7MJ2hDG1Bi8nytc1UjfCoEDwCLgCXL-f1oQpsSYZD9l9EsKHiDkQE3iiWDz_hUCCa1iVDZB9aHfmNBi42xb8nOEG3kmn-4SI9-e8Ue6V8zVx1a2j6eMlSXNkFtXqX0PmunQ_8095DX8WLu0AxMqM1yXLKVPMRBtDj2E8hBLqkSHu9RZghsMCJrcUeHRDl_g6SS2AV_3VHk2jj-Zhw1EanUe0QtiAiwSkDdldXVIyh7XR54m9mpg_kEola1EDBe4KxPOOf0Avl5dfZeXyLl-N3znKo2d9jDsku_xJ6wC06s9h8zbEMU86zRixBZXMxOR4zN4RKET56wrjRrGP2DcC_EVJkh_5F2z5rJT9R8FzE5wvPOCCHyXZYLAneB0OV1MJ_z7wvo8c4xlTzR6HqnVoEzBuJuKIFVnNgZp4uCBW9LeLiAXqoU43Ai2UJG37QvbBFAfvDd2iBBFJ72qYPNCARy86kVF-8dWxBmKTGeo4KiXd_6FnV5hwgZFYEtUAQZZdgK_jt3jCRYDamEPr7H5vKcf7G2AaEJYbjTpe0mHzIsDemt1hZ1dG0tyygO8Q4c3SGL5DWYts8K83z7cCy03y_SIOpvVKG42C9e1M2Y-8ylCzyxw5g4qVbMWsv2cZ-tjSrLEHE8fwZ23lXqURHPh8z0SvA68kZIJbdz=s328-no?authuser=0&quot" />
+  <title>Home</title>
+  <link rel="stylesheet" href="/index.css"/>
+</head>
+<body>
+  <header class="header">
+  </header>
+  <div class="bar">
+    <div class="bar__content u-centered">
+     <img class="bar__logo" src="https://lh3.googleusercontent.com/2BJnJprT0Hw6qvid7J55wGus1A7MJ2hDG1Bi8nytc1UjfCoEDwCLgCXL-f1oQpsSYZD9l9EsKHiDkQE3iiWDz_hUCCa1iVDZB9aHfmNBi42xb8nOEG3kmn-4SI9-e8Ue6V8zVx1a2j6eMlSXNkFtXqX0PmunQ_8095DX8WLu0AxMqM1yXLKVPMRBtDj2E8hBLqkSHu9RZghsMCJrcUeHRDl_g6SS2AV_3VHk2jj-Zhw1EanUe0QtiAiwSkDdldXVIyh7XR54m9mpg_kEola1EDBe4KxPOOf0Avl5dfZeXyLl-N3znKo2d9jDsku_xJ6wC06s9h8zbEMU86zRixBZXMxOR4zN4RKET56wrjRrGP2DcC_EVJkh_5F2z5rJT9R8FzE5wvPOCCHyXZYLAneB0OV1MJ_z7wvo8c4xlTzR6HqnVoEzBuJuKIFVnNgZp4uCBW9LeLiAXqoU43Ai2UJG37QvbBFAfvDd2iBBFJ72qYPNCARy86kVF-8dWxBmKTGeo4KiXd_6FnV5hwgZFYEtUAQZZdgK_jt3jCRYDamEPr7H5vKcf7G2AaEJYbjTpe0mHzIsDemt1hZ1dG0tyygO8Q4c3SGL5DWYts8K83z7cCy03y_SIOpvVKG42C9e1M2Y-8ylCzyxw5g4qVbMWsv2cZ-tjSrLEHE8fwZ23lXqURHPh8z0SvA68kZIJbdz=s328-no?authuser=0" alt="Home page">
+    <input type="checkbox" id="inpNavToggle">
+      <label class="bar__nav-toggle" for="inpNavToggle">
+        <i class="material-icons">menu</i>
+      </label>
+      <nav class="nav">
+        <a class="nav__link" href="#1">Page1</a>
+        <a class="nav__link" href="#2">Page2</a>
+        <a class="nav__link" href="#3">Page3</a>
+        <a class="nav__link" href="#4">Page4</a>
+      </nav>
+    </div>
+  </div>
+</body>
