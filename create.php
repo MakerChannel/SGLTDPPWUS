152fadf1d2f90d5771a0f33af6f3f8c7732eebea
MakerChannel <68184436+MakerChannel@users.noreply.github.com>
2021-03-19 13:54:12 +0000

    Update create.php

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <link type="text/css" rel="stylesheet" href="/create.css">
      <link rel="stylesheet" type="text/css" href="/index.css">
   </head>
